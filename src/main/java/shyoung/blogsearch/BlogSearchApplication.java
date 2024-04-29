a432d4cda6fbb06fda5f6f23426738140e6a4a60
서현영 <shyoung3324@gmail.com>
2024-04-29 18:54:14 +0900

    chore: init settings

---
+++ src/main/java/shyoung/blogsearch/BlogSearchApplication.java
@@ -0,0 +1,11 @@
+package shyoung.blogsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchApplication.class);
+    }
+}
